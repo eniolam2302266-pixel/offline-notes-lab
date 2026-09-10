47ca20e4d76eafb7e013b8ad8f0110e6fd01ade6
Eniolam2302266-pixel <eniola.m2302266@st.futminna.edu.ng>
2026-09-10 11:17:47 +0100

    Details update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -47,8 +47,8 @@ export default function Home() {
       <main>
         <p className="eyebrow">FOUNDATION TRACK</p>
         <h1>
-          Dennis Utighonom Dogonyaro <br />
-          2024/1/101431CM <br />
+          Gbadegesin Eniola Temitope <br />
+          2024/1/92157CM <br />
           Telecommunications Engineering
         </h1>
         <p className="lede">Save a note, refresh the page, then test the same experience with the network turned off.</p>
